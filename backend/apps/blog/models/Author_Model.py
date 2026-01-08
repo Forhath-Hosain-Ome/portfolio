45cbdbf6bb0a06a73e0328e13ffba67e6e540b90
Forhath-Hosain-Ome <omehasan3@gmail.com>
2026-01-09 00:04:50 +0600

    blog model v0.1

---
+++ backend/apps/blog/models/Author_Model.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from apps.core.models import TimeStampedModel, PublishableModel, SEOModel
+from apps.portfolio.models import Category, Tag
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
+
+class AuthorModel(TimeStampedModel):
+    """Author profile model."""
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='author_profile')
+    display_name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='authors/', blank=True)
+    website = models.URLField(blank=True)
+    
+    # Social links
+    twitter = models.CharField(max_length=100, blank=True)
+    linkedin = models.CharField(max_length=100, blank=True)
+    github = models.CharField(max_length=100, blank=True)
+    
+    class Meta:
+        ordering = ['display_name']
+    
+    def __str__(self):
+        return self.display_name
---
+++ backend/apps/blog/models/Blog_Post_Model.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import User
+from apps.core.models import TimeStampedModel, PublishableModel, SEOModel
+from apps.portfolio.models import Category, Tag
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
+class BlogPostModel(TimeStampedModel, PublishableModel, SEOModel):
+    """Blog post model."""
+    
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    
+    # Author
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    
+    # Images
+    featured_image = models.ImageField(upload_to='blog/%Y/%m/')
+    thumbnail = ImageSpecField(
+        source='featured_image',
+        processors=[ResizeToFill(400, 300)],
+        format='JPEG',
+        options={'quality': 85}
+    )
+    
+    # Content
+    excerpt = models.TextField(max_length=300)
+    content = models.TextField()
+    content_extended = models.TextField(blank=True)
+    
+    # Reading time
+    read_time_minutes = models.PositiveIntegerField(default=5)
+    
+    # Engagement metrics
+    views_count = models.PositiveIntegerField(default=0)
+    likes_count = models.PositiveIntegerField(default=0)
+    comments_count = models.PositiveIntegerField(default=0)
+    
+    # Featured
+    is_featured = models.BooleanField(default=False)
+    
+    # Relationships
+    categories = models.ManyToManyField(
+        Category,
+        related_name='blog_posts',
+        limit_choices_to={'type': 'blog'}
+    )
+    tags = models.ManyToManyField(Tag, related_name='blog_posts', blank=True)
+    
+    class Meta:
+        ordering = ['-published_at']
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
