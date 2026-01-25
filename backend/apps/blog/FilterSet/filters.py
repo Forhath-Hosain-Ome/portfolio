fafa43ba3ca050089a480800d849e0db45b1b4e2
Forhath Hosain Ome <omehasan3@gmail.com>
2026-01-25 17:13:35 +0600

    Custom filterset to fix

---
+++ backend/apps/blog/FilterSet/filters.py
@@ -0,0 +1,15 @@
+# # filters.py
+# import django_filters
+# from .models import BlogPostModel
+
+
+# class BlogPostFilter(django_filters.FilterSet):
+#     author = django_filters.NumberFilter(field_name="author__id")
+#     is_active = django_filters.BooleanFilter()
+#     created_after = django_filters.DateFilter(
+#         field_name="created_at", lookup_expr="gte"
+#     )
+
+#     class Meta:
+#         model = BlogPostModel
+#         fields = ["author", "is_active"]
