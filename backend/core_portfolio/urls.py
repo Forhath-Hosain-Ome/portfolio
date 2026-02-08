aba5b72f937671227e5776fc0928243a37073471
Forhath Hosain Ome-Ubuntu <omehasan3@gmail.com>
2026-02-08 23:51:16 +0600

    core url configed

--- backend/core_portfolio/urls.py
+++ backend/core_portfolio/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('apps.blog.urls')),
+    path('', include('apps.blog.urls')),
+    path('', include('apps.core.urls')),
 ]
